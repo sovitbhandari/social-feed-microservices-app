449d868b8d336a9b5a47913053082548f2ebc38d
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-07-25 13:59:02 -0400

    fixed http issues

--- client/src/SocialCommentCreate.js
+++ client/src/SocialCommentCreate.js
@@ -18,7 +18,7 @@ export default function CommentCreate({ postId, onAddComment }) {
     };
 
     try {
-      await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      await axios.post(`http://posts.com/posts/${postId}/comments`, {
         content: comment
       });
       onAddComment(newComment);
